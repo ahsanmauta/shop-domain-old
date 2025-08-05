a11f10df00df24257aa23d9b39b845b0d5951f7b
praja wibisono <ahsanmauta@proton.me>
2025-08-05 12:08:19 +0700

    Create Country.php

---
+++ app/Models/Country.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Country extends Authenticatable
+{
+  use HasFactory, Notifiable;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'iso',
+	'name',
+    'nicename',
+	'phonecode',
+  ];
+  
+  protected $table = 'country';
+
+
+}
