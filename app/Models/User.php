a9e9528f038f5c78de3c4459b9d0093ba350b54f
praja wibisono <ahsanmauta@proton.me>
2025-08-05 12:08:26 +0700

    Create User.php

---
+++ app/Models/User.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+  use HasFactory, Notifiable;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'name',
+	'firstname',
+	'lastname',
+    'email',
+    'password',
+	'country',
+	'mobile',
+	'address',
+	'state',
+	'zipcode',
+	'city',
+	'active',
+	'profile_photo_url',
+	'saldo',
+	'kurs',
+	'admin',
+  ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
+
+  /**
+   * Get the attributes that should be cast.
+   *
+   * @return array<string, string>
+   */
+  protected function casts(): array
+  {
+    return [
+      'email_verified_at' => 'datetime',
+      'password' => 'hashed',
+    ];
+  }
+  
+  public function isAdmin()
+  {
+		return $this->admin ? true : false; // this looks for an admin column in your users table
+  }
+  
+}
