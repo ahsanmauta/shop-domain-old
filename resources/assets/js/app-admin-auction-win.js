778e07a004baa19544e27fc2344b6b181ea6bcf4
praja wibisono <ahsanmauta@proton.me>
2025-08-05 13:36:00 +0700

    Create app-admin-auction-win.js

---
+++ resources/assets/js/app-admin-auction-win.js
@@ -0,0 +1,351 @@
+/**
+ * Page User List
+ */
+
+'use strict';
+
+// Datatable (jquery)
+$(function () {
+  let borderColor, bodyBg, headingColor;
+
+  if (isDarkStyle) {
+    borderColor = config.colors_dark.borderColor;
+    bodyBg = config.colors_dark.bodyBg;
+    headingColor = config.colors_dark.headingColor;
+  } else {
+    borderColor = config.colors.borderColor;
+    bodyBg = config.colors.bodyBg;
+    headingColor = config.colors.headingColor;
+  }
+
+  // Variable declaration for table
+  var dt_user_table = $('.datatables-users'),
+    select2 = $('.select2'),
+    userView = baseUrl + 'admin/adminauction/bidshow',
+    statusObj = {
+      1: { title: 'Pending', class: 'bg-label-warning' },
+      2: { title: 'Active', class: 'bg-label-success' },
+      3: { title: 'Inactive', class: 'bg-label-secondary' }
+    };
+
+  if (select2.length) {
+    var $this = select2;
+    $this.wrap('<div class="position-relative"></div>').select2({
+      placeholder: 'Select Country',
+      dropdownParent: $this.parent()
+    });
+  }
+
+  // Users datatable
+  if (dt_user_table.length) {
+    var dt_user = dt_user_table.DataTable({
+      //ajax: assetsPath + 'json/user-list.json', // JSON file to add data
+	  ajax: {
+        url: baseUrl + 'admin/bidwin'
+      },
+      columns: [
+        // columns according to JSON
+        { data: 'id' },
+        { data: 'domain' },
+        { data: 'category' },
+        { data: 'endtime' },
+        { data: 'price' },
+        { data: 'iduser' },
+		{ data: 'bidstatus' },
+        { data: 'action' }
+      ],
+      columnDefs: [
+        {
+          // For Responsive
+          className: 'control',
+          searchable: false,
+          orderable: false,
+          responsivePriority: 2,
+          targets: 0,
+          render: function (data, type, full, meta) {
+            return '';
+          }
+        },
+        {
+          // For Checkboxes
+          targets: 1,
+          orderable: false,
+          checkboxes: {
+            selectAllRender: '<input type="checkbox" class="form-check-input">'
+          },
+          render: function () {
+            return '<input type="checkbox" class="dt-checkboxes form-check-input" >';
+          },
+          searchable: false
+        },
+        {
+          targets: 2,
+          responsivePriority: 4,
+          render: function (data, type, full, meta) {
+            var $name = full['domain'],
+              $email = full['category'];
+            
+            // Creates full output for row
+            var $row_output =
+              '<div class="d-flex justify-content-start align-items-center user-name">' +
+              '<div class="d-flex flex-column">' +
+              '<a href="' +
+              userView +
+              '" class="text-heading text-truncate"><span class="fw-medium">' +
+              $name +
+              '</span></a>' +
+              '<small>' +
+              $email +
+              '</small>' +
+              '</div>' +
+              '</div>';
+            return $row_output;
+          }
+        },
+        {
+          targets: 3,
+          render: function (data, type, full, meta) {
+            var $endtime = full['endtime'];
+            return (
+              "<span class='text-truncate d-flex align-items-center text-heading'>" +
+              $endtime +
+              '</span>'
+            );
+          }
+        },
+        {
+          targets: 4,
+          render: function (data, type, full, meta) {
+            var $price = full['price'];
+
+            return '<span class="text-heading">$ ' + $price + '</span>';
+          }
+        },
+        {
+          targets: 5,
+          render: function (data, type, full, meta) {
+            var $name = full['name'],
+				$firstname = full['firstname'],
+				$lastname = full['lastname'];
+
+            return '<span class="text-heading">' + $firstname + ' ' + $lastname + '</span>';
+          }
+        },
+        {
+          targets: 6,
+          render: function (data, type, full, meta) {
+            var $bidstatus = full['bidstatus'];
+			var roleBadgeObj = {
+              1: '<span class="badge bg-secondary bg-glow">open</span>',
+              2: '<span class="badge bg-warning bg-glow">win</span>',
+              3: '<span class="badge bg-success bg-glow">paid</span>',
+            };
+            return roleBadgeObj[$bidstatus];
+          }
+        },
+        {
+          // Actions
+          targets: -1,
+          title: 'Actions',
+          searchable: false,
+          orderable: false,
+          render: function (data, type, full, meta) {
+            return (
+              '<div class="d-flex align-items-center">' +
+              '<a href="javascript:;" class="btn btn-icon btn-text-secondary waves-effect waves-light rounded-pill delete" data-id="'+full['id']+'" data-domain="'+full['domain']+'" data-bs-toggle="modal" data-bs-target="#basicModal" ><i class="ti ti-trash ti-md"></i></a>' +
+              '<a href="' + userView + '/' + full['idauction'] + '" class="btn btn-icon btn-text-secondary waves-effect waves-light rounded-pill"><i class="ti ti-eye ti-md"></i></a>' +
+              '<a href="javascript:;" class="btn btn-icon btn-text-secondary waves-effect waves-light rounded-pill dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical ti-md"></i></a>' +
+              '<div class="dropdown-menu dropdown-menu-end m-0">' +
+              '</div>' +
+              '</div>'
+            );
+          }
+        }
+      ],
+      order: [[2, 'desc']],
+      dom:
+        '<"row"' +
+        '<"col-md-2"<"ms-n2"l>>' +
+        '<"col-md-10"<"dt-action-buttons text-xl-end text-lg-start text-md-end text-start d-flex align-items-center justify-content-end flex-md-row flex-column mb-6 mb-md-0 mt-n6 mt-md-0"fB>>' +
+        '>t' +
+        '<"row"' +
+        '<"col-sm-12 col-md-6"i>' +
+        '<"col-sm-12 col-md-6"p>' +
+        '>',
+      language: {
+        sLengthMenu: '_MENU_',
+        search: '',
+        searchPlaceholder: 'Search Bid',
+        paginate: {
+          next: '<i class="ti ti-chevron-right ti-sm"></i>',
+          previous: '<i class="ti ti-chevron-left ti-sm"></i>'
+        }
+      },
+      // Buttons with Dropdown
+      buttons: [
+        {
+          extend: 'collection',
+          className: 'btn btn-label-secondary dropdown-toggle mx-4 waves-effect waves-light',
+          text: '<i class="ti ti-upload me-2 ti-xs"></i>Export',
+          buttons: [
+            {
+              extend: 'print',
+              text: '<i class="ti ti-printer me-2" ></i>Print',
+              className: 'dropdown-item',
+              exportOptions: {
+                columns: [1, 2, 3, 4, 5],
+                // prevent avatar to be print
+                format: {
+                  body: function (inner, coldex, rowdex) {
+                    if (inner.length <= 0) return inner;
+                    var el = $.parseHTML(inner);
+                    var result = '';
+                    $.each(el, function (index, item) {
+                      if (item.classList !== undefined && item.classList.contains('user-name')) {
+                        result = result + item.lastChild.firstChild.textContent;
+                      } else if (item.innerText === undefined) {
+                        result = result + item.textContent;
+                      } else result = result + item.innerText;
+                    });
+                    return result;
+                  }
+                }
+              },
+              customize: function (win) {
+                //customize print view for dark
+                $(win.document.body)
+                  .css('color', headingColor)
+                  .css('border-color', borderColor)
+                  .css('background-color', bodyBg);
+                $(win.document.body)
+                  .find('table')
+                  .addClass('compact')
+                  .css('color', 'inherit')
+                  .css('border-color', 'inherit')
+                  .css('background-color', 'inherit');
+              }
+            },
+            {
+              extend: 'csv',
+              text: '<i class="ti ti-file-text me-2" ></i>Csv',
+              className: 'dropdown-item',
+              exportOptions: {
+                columns: [1, 2, 3, 4, 5],
+                // prevent avatar to be display
+                format: {
+                  body: function (inner, coldex, rowdex) {
+                    if (inner.length <= 0) return inner;
+                    var el = $.parseHTML(inner);
+                    var result = '';
+                    $.each(el, function (index, item) {
+                      if (item.classList !== undefined && item.classList.contains('user-name')) {
+                        result = result + item.lastChild.firstChild.textContent;
+                      } else if (item.innerText === undefined) {
+                        result = result + item.textContent;
+                      } else result = result + item.innerText;
+                    });
+                    return result;
+                  }
+                }
+              }
+            },
+            {
+              extend: 'excel',
+              text: '<i class="ti ti-file-spreadsheet me-2"></i>Excel',
+              className: 'dropdown-item',
+              exportOptions: {
+                columns: [1, 2, 3, 4, 5],
+                // prevent avatar to be display
+                format: {
+                  body: function (inner, coldex, rowdex) {
+                    if (inner.length <= 0) return inner;
+                    var el = $.parseHTML(inner);
+                    var result = '';
+                    $.each(el, function (index, item) {
+                      if (item.classList !== undefined && item.classList.contains('user-name')) {
+                        result = result + item.lastChild.firstChild.textContent;
+                      } else if (item.innerText === undefined) {
+                        result = result + item.textContent;
+                      } else result = result + item.innerText;
+                    });
+                    return result;
+                  }
+                }
+              }
+            },
+            {
+              extend: 'pdf',
+              text: '<i class="ti ti-file-code-2 me-2"></i>Pdf',
+              className: 'dropdown-item',
+              exportOptions: {
+                columns: [1, 2, 3, 4, 5],
+                // prevent avatar to be display
+                format: {
+                  body: function (inner, coldex, rowdex) {
+                    if (inner.length <= 0) return inner;
+                    var el = $.parseHTML(inner);
+                    var result = '';
+                    $.each(el, function (index, item) {
+                      if (item.classList !== undefined && item.classList.contains('user-name')) {
+                        result = result + item.lastChild.firstChild.textContent;
+                      } else if (item.innerText === undefined) {
+                        result = result + item.textContent;
+                      } else result = result + item.innerText;
+                    });
+                    return result;
+                  }
+                }
+              }
+            },
+            {
+              extend: 'copy',
+              text: '<i class="ti ti-copy me-2" ></i>Copy',
+              className: 'dropdown-item',
+              exportOptions: {
+                columns: [1, 2, 3, 4, 5],
+                // prevent avatar to be display
+                format: {
+                  body: function (inner, coldex, rowdex) {
+                    if (inner.length <= 0) return inner;
+                    var el = $.parseHTML(inner);
+                    var result = '';
+                    $.each(el, function (index, item) {
+                      if (item.classList !== undefined && item.classList.contains('user-name')) {
+                        result = result + item.lastChild.firstChild.textContent;
+                      } else if (item.innerText === undefined) {
+                        result = result + item.textContent;
+                      } else result = result + item.innerText;
+                    });
+                    return result;
+                  }
+                }
+              }
+            }
+          ]
+        }
+      ]
+    });
+  }
+
+  // Delete Record
+  $('.datatables-users tbody').on('click', '.delete-record', function () {
+    dt_user.row($(this).parents('tr')).remove().draw();
+  });
+		
+	$('.datatables-users tbody').on('click', '.delete', function () {
+         let id = $(this).attr('data-id');
+         $('#id').val(id);
+		 let domain = $(this).attr('data-domain');
+		 $('#domain').val(domain);
+    });	
+  
+  
+  // Filter form control to default size
+  // ? setTimeout used for multilingual table initialization
+  setTimeout(() => {
+    $('.dataTables_filter .form-control').removeClass('form-control-sm');
+    $('.dataTables_length .form-select').removeClass('form-select-sm');
+  }, 300);
+});
+
+
