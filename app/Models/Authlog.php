c74b94bca15d18c76b741a08b983c86d0bc4f1dd
praja wibisono <ahsanmauta@proton.me>
2025-08-05 12:08:14 +0700

    Create Authlog.php

---
+++ app/Models/Authlog.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Authlog extends Authenticatable
+{
+  use HasFactory, Notifiable;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'event_name',
+	'email',
+    'user_id',
+	'ip_address',
+	'user_agent',
+	'context',
+  ];
+  
+  protected $table = 'authentication_logs';
+
+
+}
