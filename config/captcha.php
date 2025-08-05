d8581b1c48de5535fb529f76210c5863d80654b3
praja wibisono <ahsanmauta@proton.me>
2025-08-05 12:09:01 +0700

    Create captcha.php

---
+++ config/captcha.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'default'   => [
+        'length'    => 4,
+        'width'     => 160,
+        'height'    => 36,
+        'quality'   => 90,
+        'math'      => false,  //Enable Math Captcha
+        'expire'    => 60,    //Captcha expiration
+    ],
+    // ...
+];
+?>
