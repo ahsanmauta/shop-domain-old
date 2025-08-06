80d96e40c29e5de58fd3cea2335cc1baaa46b0a3
praja wibisono <ahsanmauta@proton.me>
2025-08-06 08:26:57 +0700

    Create auction_bid.blade.php

---
+++ resources/views/content/admins/auction_bid.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts/layoutMaster')
+
+@section('title', 'User List - Pages')
+
+@section('vendor-style')
+@vite([
+  'resources/assets/vendor/libs/datatables-bs5/datatables.bootstrap5.scss',
+  'resources/assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.scss',
+  'resources/assets/vendor/libs/datatables-buttons-bs5/buttons.bootstrap5.scss',
+  'resources/assets/vendor/libs/select2/select2.scss',
+  'resources/assets/vendor/libs/@form-validation/form-validation.scss'
+])
+@endsection
+
+@section('vendor-script')
+@vite([
+  'resources/assets/vendor/libs/moment/moment.js',
+  'resources/assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js',
+  'resources/assets/vendor/libs/select2/select2.js',
+  'resources/assets/vendor/libs/@form-validation/popular.js',
+  'resources/assets/vendor/libs/@form-validation/bootstrap5.js',
+  'resources/assets/vendor/libs/@form-validation/auto-focus.js',
+  'resources/assets/vendor/libs/cleavejs/cleave.js',
+  'resources/assets/vendor/libs/cleavejs/cleave-phone.js'
+])
+@endsection
+
+@section('page-script')
+@vite('resources/assets/js/app-admin-auction-bid.js')
+@endsection
+
+@section('content')
+
+
+<!-- Users List Table -->
+<div class="card">
+  <div class="card-header border-bottom">
+    <h5 class="card-title mb-0">ALL BID AUCTIONS</h5>
+    <div class="d-flex justify-content-between align-items-center row pt-4 gap-4 gap-md-0">
+      <div class="col-md-4 user_role"></div>
+      <div class="col-md-4 user_plan"></div>
+      <div class="col-md-4 user_status"></div>
+    </div>
+  </div>
+  <div class="card-datatable table-responsive">
+    <table class="datatables-users table">
+      <thead class="border-top">
+        <tr>
+          <th></th>
+          <th></th>
+          <th>Domain</th>
+          <th>End Time</th>
+          <th>Price</th>
+          <th>User</th>
+		  <th>Status</th>
+          <th>Actions</th>
+        </tr>
+      </thead>	  
+    </table>
+  </div>
+  
+          <!-- Modal -->
+		  <form action="{{route('admin.admin-destroybid','1')}}" method="post">
+		  @csrf @method('POST') 
+          <div class="modal fade" id="basicModal" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel1">Delete Confirmation</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <div class="row">
+                    <div class="col mb-4">
+                      <label for="nameBasic" class="form-label">Domain</label>
+					  <input type="hidden" id="id" name="id" >
+                      <input type="text" id="domain" name="domain" class="form-control" placeholder="Enter Domain">
+                    </div>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary">Yes delete</button>
+                </div>
+              </div>
+            </div>
+          </div>
+		  </form>
+		  
+</div>
+
+@endsection
